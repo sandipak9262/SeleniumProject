94ff9e99e6f8437195ec2db36f5fd6403e42d375
Dipak Musale <45799049+sandipak9262@users.noreply.github.com>
2022-07-28 13:25:10 +0530

    Update Demo.java

--- Automation/src/webelement_methods/Demo.java
+++ Automation/src/webelement_methods/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver","F:\\Selenium\\geckodriver.exe");  
 		WebDriver driver=new FirefoxDriver();
-		driver.get("https://www.facebook.com");
+		driver.get("https://www.instagram.com");
 	}
 
 }
